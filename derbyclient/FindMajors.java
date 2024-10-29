46983a03533ed4f31338be59ae0ac13b7180dee4
Arpit Saxena <arpit.saxena2000@yahoo.in>
2024-10-29 13:51:21 +0530

    Display derbyclient FindMajors result in JTable

--- derbyclient/FindMajors.java
+++ derbyclient/FindMajors.java
@@ -2,7 +2,51 @@ import java.sql.*;
 import java.util.Scanner;
 import org.apache.derby.jdbc.ClientDataSource;
 
+import javax.swing.*;
+import javax.swing.table.AbstractTableModel;
+import javax.swing.table.TableModel;
+import java.awt.BorderLayout;
+
 public class FindMajors {
+   private static void displayJTable(JTable table) {
+      JFrame frame = new JFrame("JTable Test Display");
+      JPanel panel = new JPanel();
+      panel.setLayout(new BorderLayout());
+      JScrollPane scrollPane = new JScrollPane(table);
+      table.setFillsViewportHeight(true);
+
+      panel.add(scrollPane, BorderLayout.CENTER);
+      frame.getContentPane().add(panel);
+
+      frame.pack();
+      frame.setVisible(true);
+   }
+
+   private static void displayData(ResultSet rs) throws SQLException {
+      rs.last();
+      int rowCount = rs.getRow();
+      rs.beforeFirst();
+      int columnCount = rs.getMetaData().getColumnCount();
+
+      TableModel dataModel = new AbstractTableModel() {
+         public int getColumnCount() { return columnCount; }
+         public int getRowCount() { return rowCount; }
+         public Object getValueAt(int rowIndex, int columnIndex) {
+            try {
+               rs.absolute(rowIndex + 1);
+               Object o = rs.getObject(columnIndex + 1);
+               return o;
+            } catch (SQLException e) {
+               System.out.println(e.toString());
+               return null;
+            }
+         }
+
+      };
+
+      displayJTable(new JTable(dataModel));
+   }
+
    public static void main(String[] args) {
       System.out.print("Enter a department name: ");
       Scanner sc = new Scanner(System.in);
@@ -16,17 +60,12 @@ public class FindMajors {
       ClientDataSource ds = new ClientDataSource();
       ds.setServerName("localhost");
       ds.setDatabaseName("studentdb");
-      try (Connection conn = ds.getConnection();
-            Statement stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery(qry)) {
+      try (Connection conn = ds.getConnection()) {
+         conn.setAutoCommit(false);
+         Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+         ResultSet rs = stmt.executeQuery(qry);
 
-         System.out.println("Here are the " + major + " majors");
-         System.out.println("Name\tGradYear");
-         while (rs.next()) {
-            String sname = rs.getString("sname");
-            int gradyear = rs.getInt("gradyear");
-            System.out.println(sname + "\t" + gradyear);
-         }
+         displayData(rs);
       }
       catch(Exception e) {
          e.printStackTrace();
